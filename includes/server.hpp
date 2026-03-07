a3d3b51222c8fae2146e9d08caeb99dd87f017a4
Mohamed Amine Wardane <mowardan@c3r1p13.1337.ma>
2026-03-07 01:54:55 +0000

    errors

--- includes/server.hpp
+++ includes/server.hpp
@@ -1,24 +1,7 @@
 #ifndef SERVER_HPP
-# define SERVER_HPP
+#define SERVER_HPP
 
-
+// Just include the main header - class is defined there
 #include "ft_irc.hpp"
-#include <vector>
-#include <map>
 
-class client;
-
-class Server
-{
-    private:
-    int server_fd;
-    int port;
-    std::string password;
-    std::vector<struct pollfd> poll_fds;
-    std::map<int, client*> clients;
-    public:
-        Server(int port, const std::string& password);
-        ~Server();
-        void run();
-};
 #endif
