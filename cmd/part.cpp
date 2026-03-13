f2562c358e8ed0c70f422766cf996ef106d59acb
Hicham Elfatihi <omarchouki01@gmail.com>
2026-03-13 12:53:21 +0000

    ramadan vibe

--- cmd/part.cpp
+++ cmd/part.cpp
@@ -5,6 +5,7 @@ void Server::handlePart(int client_fd, const std::vector<std::string> &tokens)
     if(tokens.size() > 2 || tokens.size() < 3)
         throw std::runtime_error("Usage: PART <channel> :<msg>\r\n");
     std::string channel_name = tokens[1];
+    std::string msg = "";
     checkChannelName(channel_name);
     checkChannelExist(channel_name);
     checkIsMember(channel_name, clientsFds[client_fd], clientsFds[client_fd]->getNick());
@@ -12,15 +13,13 @@ void Server::handlePart(int client_fd, const std::vector<std::string> &tokens)
         channels[channel_name]->removeInvite(clientsFds[client_fd]);
     if(channels[channel_name]->getOperators().count(clientsFds[client_fd]))
         channels[channel_name]->removeOperator(clientsFds[client_fd]);
+    else if(tokens.size() == 3)
+        msg =  tokens[2];
+    channels[channel_name]->brodcastMsg(":" + clientsFds[client_fd]->getNick() + " PART "+ channel_name + " :" + msg, NULL);
     channels[channel_name]->removeClient(clientsFds[client_fd]);
     if(channels[channel_name]->channelEmpty())
     {
         delete channels[channel_name];
         channels.erase(channel_name);
-    }
-    else if(tokens.size() == 3)
-    {
-        std::string msg =  ":" + clientsFds[client_fd]->getNick() + "!"+ clientsFds[client_fd]->getUsername() + " PART "+ channel_name + " :" + tokens[2] + "\r\n";
-        channels[channel_name]->brodcastMsg(msg, clientsFds[client_fd]);
     }
 }
